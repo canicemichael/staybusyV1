c1143bcadefb112124c676bd5146c676d9864cfc
canicemichael <canicemichael@gmail.com>
2022-10-15 12:43:13 +0100

    update 10/15

---
+++ config/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  aws: {
+    key: "AKIAZGSBN36XKRQFXP27",
+    secret: "3Tka5ecfVflDb56QQNpeax3UYuRH+R9RgQWeoCWs",
+    ses: {
+      from: {
+        // replace with an actual email address
+        default:
+          '"ezugwuchukwuebukka@gmail.com" <noreply@ezugwuchukwuebukka@gmail.com>',
+      },
+      // e.g. us-west-2
+      region: "US East (N. Virginia)",
+    },
+  },
+};
